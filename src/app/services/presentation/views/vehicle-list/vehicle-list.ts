d6933424b0b1be76cf7357b615784bce06ff5c96
GZ-99 <gabrielvilchezvite@gmail.com>
2026-05-20 00:50:01 -0500

    feat(app): Add vehicle-list.ts, vehicle-list.html and vehicle-list.css.

--- src/app/services/presentation/views/vehicle-list/vehicle-list.ts
+++ src/app/services/presentation/views/vehicle-list/vehicle-list.ts
@@ -1,9 +1,29 @@
 import { Component } from '@angular/core';
+import {inject} from '@angular/core';
+import {Router} from '@angular/router';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTableModule} from '@angular/material/table';
+import {ServiceStore} from '../../../application/service-store';
+import {MatError} from '@angular/material/form-field';
+import {MatProgressSpinner} from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-vehicle-list',
-  imports: [],
+  imports: [MatTableModule, MatButtonModule, MatError, MatProgressSpinner],
   templateUrl: './vehicle-list.html',
   styleUrl: './vehicle-list.css',
 })
-export class VehicleList {}
+export class VehicleList {
+  readonly store = inject(ServiceStore);
+  protected router = inject(Router);
+
+  displayedColumns: string[] = ['id', 'make', 'model', 'mileageKm', 'dailyRate', 'vehicleType', 'status', 'actions'];
+
+  editVehicle(id: number) {
+    this.router.navigate(['services/vehicles/edit', id]).then();
+  }
+
+  deleteVehicle(id: number) {
+    this.store.deleteVehicle(id);
+  }
+}
