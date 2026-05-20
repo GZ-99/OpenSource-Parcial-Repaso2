8e09a3b95dd905cfb9110d96c6cdf6a46948d948
GZ-99 <gabrielvilchezvite@gmail.com>
2026-05-19 21:42:22 -0500

    feat(api): Add api-endpoint.

---
+++ src/app/services/infrastructure/incidents-api-endpoint.ts
@@ -0,0 +1,17 @@
+import {BaseApiEndpoint} from '../../shared/infrastructure/base-api-endpoint';
+import {Incident} from '../domain/model/incident.entity';
+import {IncidentResource, IncidentsResponse} from './incidents-response';
+import {IncidentAssembler} from './incident-assembler';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../../../environments/environment';
+
+export class IncidentsApiEndpoint extends BaseApiEndpoint<Incident, IncidentResource, IncidentsResponse, IncidentAssembler> {
+  /**
+   * Creates an instance of IncidentsApiEndpoint.
+   * @param http - The HttpClient to be used for making API requests.
+   */
+  constructor(http: HttpClient) {
+    super(http, `${environment.platformProviderApiBaseUrl}${environment.platformProviderIncidentsEndpointPath}`,
+      new IncidentAssembler());
+  }
+}
---
+++ src/app/services/infrastructure/vehicles-api-endpoint.ts
@@ -0,0 +1,17 @@
+import {BaseApiEndpoint} from '../../shared/infrastructure/base-api-endpoint';
+import {Vehicle} from '../domain/model/vehicle.entity';
+import {VehiclesResponse, VehicleResource} from './vehicles-response';
+import {VehicleAssembler} from './vehicle-assembler';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../../../environments/environment';
+
+export class VehiclesApiEndpoint extends BaseApiEndpoint<Vehicle, VehicleResource, VehiclesResponse, VehicleAssembler> {
+  /**
+   * Creates an instance of CategoriesApiEndpoint.
+   * @param http - The HttpClient to be used for making API requests.
+   */
+  constructor(http: HttpClient) {
+    super(http, `${environment.platformProviderApiBaseUrl}${environment.platformProviderVehiclesEndpointPath}`,
+      new VehicleAssembler());
+  }
+}
