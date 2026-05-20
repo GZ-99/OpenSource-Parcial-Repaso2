90de11068ccca6a5af148d0064d28e9c2922c852
GZ-99 <gabrielvilchezvite@gmail.com>
2026-05-20 00:27:28 -0500

    feat(layout): Add layout.ts, layout.html and layout.css.

--- src/app/shared/presentation/components/layout/layout.ts
+++ src/app/shared/presentation/components/layout/layout.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
+import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
+import {MatToolbar, MatToolbarRow} from '@angular/material/toolbar';
+import {MatButton} from '@angular/material/button';
+import {TranslatePipe} from '@ngx-translate/core';
+import {LanguageSwitcher} from '../language-switcher/language-switcher';
+import {FooterContent} from '../footer-content/footer-content';
 
 @Component({
   selector: 'app-layout',
-  imports: [],
+  imports: [RouterOutlet, RouterLink, MatToolbarRow, MatToolbar, MatButton, RouterLinkActive,
+    TranslatePipe, LanguageSwitcher, FooterContent],
   templateUrl: './layout.html',
   styleUrl: './layout.css',
 })
-export class Layout {}
+export class Layout {
+  options = [
+    {link: '/home', label: 'option.home'},
+    {link: '/new', label: 'option.new'},
+    {link: '/services/vehicles', label: 'option.vehicles'},
+    {link: '/services/rentals', label: 'option.rentals'},
+    {link: '/services/incidents', label: 'option.incidents'}
+  ];
+}
