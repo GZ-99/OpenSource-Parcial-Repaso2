ddc1ef2be20b01eb20fd5532ad1c21392431fea1
GZ-99 <gabrielvilchezvite@gmail.com>
2026-05-20 12:36:46 -0500

    feat(app): Add incident-list.ts, incident-list.html and incident-list.css.

--- src/app/services/presentation/views/incident-list/incident-list.ts
+++ src/app/services/presentation/views/incident-list/incident-list.ts
@@ -1,9 +1,31 @@
 import { Component } from '@angular/core';
+import {inject} from '@angular/core';
+import {ServiceStore} from '../../../application/service-store';
+import {Router} from '@angular/router';
+import {MatError} from '@angular/material/form-field';
+import {MatCell, MatCellDef, MatColumnDef, MatHeaderCell, MatHeaderCellDef,
+  MatHeaderRow, MatHeaderRowDef, MatRow, MatRowDef, MatTable} from '@angular/material/table';
+import {MatButton} from '@angular/material/button';
+import {MatProgressSpinner} from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-incident-list',
-  imports: [],
+  imports: [MatError, MatTable, MatHeaderCellDef, MatCellDef, MatColumnDef, MatHeaderCell,
+    MatCell, MatHeaderRowDef, MatRowDef, MatButton, MatHeaderRow, MatRow, MatProgressSpinner],
   templateUrl: './incident-list.html',
   styleUrl: './incident-list.css',
 })
-export class IncidentList {}
+export class IncidentList {
+  readonly store = inject(ServiceStore);
+  protected router = inject(Router);
+
+  displayedColumns: string[] = ['id', 'vehicleId', 'rentalId', 'incidentType', 'registeredAt', 'estimatedRepairCost', 'priority', 'actions'];
+
+  editIncident(id: number) {
+    this.router.navigate(['services/incidents/edit', id]).then();
+  }
+
+  deleteIncident(id: number) {
+    this.store.deleteIncident(id);
+  }
+}
